1922fb5141249f4564e86ae1fae09ca330b409c6
kaarthik108 <kaarthikandavar@gmail.com>
2024-03-25 13:23:57 +1300

    refractor RevenueCard

--- components/RevenueCard.tsx
+++ components/RevenueCard.tsx
@@ -1,11 +1,25 @@
 // components/RevenueCard.tsx
 import { fetchRevenueData } from "@/app/actions";
 import { groupByMonth } from "@/lib/utils";
+import { DollarSign } from "lucide-react";
 import { RevenueOverTime } from "./charts/sparkChart";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export async function RevenueCard() {
   const revenueData = await fetchRevenueData();
   const formattedData = revenueData ? groupByMonth(revenueData) : [];
   console.log(formattedData);
-  return <RevenueOverTime chartData={formattedData} />;
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+        <DollarSign className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">$45,231.89</div>
+        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+        <RevenueOverTime chartData={formattedData} />
+      </CardContent>
+    </Card>
+  );
 }
--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -26,21 +26,7 @@ export async function Dashboard() {
       <TopHeader />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total Revenue
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">$45,231.89</div>
-              <p className="text-xs text-muted-foreground">
-                +20.1% from last month
-              </p>
-              <RevenueCard />
-            </CardContent>
-          </Card>
+          <RevenueCard />
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
